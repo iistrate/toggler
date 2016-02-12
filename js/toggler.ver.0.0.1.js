1bdc3e5eaa8acadb32e32a6ee7486dffd5d5b32d
Ioan G. Istrate <ioan@htmlthis.com>
2016-02-12 13:25:16 -0500

    include custom positioning

--- js/toggler.ver.0.0.1.js
+++ js/toggler.ver.0.0.1.js
@@ -1,11 +1,12 @@
 (function($) {
     //action selector, area to toggle
-    function Toggler(handle, target) {
+    function Toggler(handle, target, enforcePos) {
 
         var handler = $(handle);
         var area    = $(target);
         var shown   = false;
         var timer   = 20;
+        var isActivated = false;
 
         var setupEvents = function () {
             handler.on('click', toggleTarget);
@@ -34,20 +35,35 @@
                 }, timer);
             }
         };
-
+        var enforcePosition = function(enforcePos.relativeTo, enforcePos.offset, enforcePos.formula) {
+            this.relativeTo = jQuery(enforcePos.relativeTo || 'body').first();
+            this.handlerTopPos = this.relativeTo.offset().top;
+            this.formula = enforcePos.formula || this.handlerTopPos - area.height();
+            this.offset = enforcePos.offset || this.formula;
+            area.css(
+                {
+                    top:this.offset+'px'
+                }
+            );
+        };
         var toggleTarget = function (e) {
             if (typeof e != 'undefined') {
                 if (e.type == 'mouseleave' && !shown)
                     return;
             }
+            typeof enforcePos !== 'undefined' : enforcePosition(null, null) : console.log('No positioning info given');
             area.stop().toggle();
             shown = !shown;
             return false;
         };
-
+        this.isActive = function() {
+            return isActivated;
+        }    
         this.init = function () {
             setupEvents();
+            isActivated = true;
         };
+    
     }
 
     new Toggler('handler selector', 'box selector').init();
